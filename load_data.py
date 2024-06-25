a73891e58e741aed6e4ba6ab019006d8d0bd5127
Anselme Emmanuel HOUNKPONOU <91261790+Hounkponou@users.noreply.github.com>
2024-06-25 11:52:17 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,8 +1,8 @@
 def load_data():
     import pandas as pd
-    d1=pd.read_csv('euromillions_2.csv')
-    d2=pd.read_csv('euromillions_201902.csv')
-    d3=pd.read_csv('euromillions_202002.csv')
+    d1=pd.read_csv('euromillions_2.csv', sep=";", encoding='latin-1')
+    d2=pd.read_csv('euromillions_201902.csv', sep=";", encoding='latin-1')
+    d3=pd.read_csv('euromillions_202002.csv', sep=";", encoding='latin-1')
     data_list=[d1,d2,d3]
     # Concatenate the DataFrames to create the final DataFrame
     data = pd.concat(data_list, ignore_index=True)[['jour_de_tirage', 'date_de_tirage', 'boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']]
