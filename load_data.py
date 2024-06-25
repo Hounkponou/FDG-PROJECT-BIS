42d5fcd60a3f821678d242139a069a3240ba7ee9
Anselme Emmanuel HOUNKPONOU <91261790+Hounkponou@users.noreply.github.com>
2024-06-25 11:56:01 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,16 +1,20 @@
 def load_data():
     import pandas as pd
-    d1=pd.read_csv('euromillions_2.csv', sep=";", encoding='latin-1')
-    d2=pd.read_csv('euromillions_201902.csv', sep=";", encoding='latin-1')
-    d3=pd.read_csv('euromillions_202002.csv', sep=";", encoding='latin-1')
-    data_list=[d1,d2,d3]
-    # Concatenate the DataFrames to create the final DataFrame
-    data = pd.concat(data_list, ignore_index=True)[['jour_de_tirage', 'date_de_tirage', 'boule_1', 'boule_2', 'boule_3', 'boule_4', 'boule_5', 'etoile_1', 'etoile_2']]
+    import io
+    import os
+    path = 'data/'
+    file_list = []
+    List = []
+
+    for root, folders, files in os.walk(path):
+        for file in files:
+            file_list.append(os.path.join(root, file))
+
+    for filename in file_list:
+        df = pd.read_csv(filename, sep=";", encoding='latin-1')
+        List.append(df)
+
+    # Concaténer les fichiers pour créer le DataFrame final
+    data = pd.concat(List, ignore_index=True)[['jour_de_tirage', 'date_de_tirage', 'boule_1', 'boule_2', 'boule_3', 'boule_4','boule_5', 'etoile_1','etoile_2']]
 
     return data
-
-# Example usage in Streamlit
-#st.title('Load Data from GitHub')
-#data = load_data()
-#st.write(data)
-
